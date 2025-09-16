a9d55084925a4947e329737227125880dafee4c9
Alvin K <ak.mbuthia98@gmail.com>
2025-09-16 16:06:20 +0300

    Add files via upload

---
+++ api_project/api/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Book
+from .serializers import BookSerializer
+
+# Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+from rest_framework import viewsets  # Import viewsets from DRF
+from .models import Book  # Import the Book model
+from .serializers import BookSerializer  # Import the BookSerializer
+
+# Define the BookViewSet using ModelViewSet
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()  # Set the queryset to all Book instances
+    serializer_class = BookSerializer  # Set the serializer class to BookSerializer
+
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]  # Restrict access to authenticated users
+
